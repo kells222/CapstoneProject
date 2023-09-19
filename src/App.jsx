f575308a6ccb4bd263b14815b4a3ac05707942c3
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-09-18 18:36:56 -0700

    cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import Posts from './components/Posts';
 import Filter from './components/Filter';
+// import { CartContext, CartProvider } from './components/Cart';
 import Checkout from './components/Checkout';
+
 
 
 function App() {
@@ -33,7 +35,9 @@ function App() {
       <Route path='/filter' element={<Filter/>}/>
       <Route path='/' element={<Posts/>}/>
       <Route path='/home' element={<Posts/>}/>
+      {/* <Route path='/checkout' element={<CartProvider/>} cartContext={<CartContext/>} /> */}
       <Route path='/checkout' element={<Checkout/>}/>
+
       <Route path='/login' element={<Login setToken={setToken}/>} />
       <Route path='/signup' element={<Signup token={token} setToken={setToken}/>} />
 
