b69f5b7db7940cfba3af74413170c4afce27c777
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-09-23 22:18:58 -0700

    cart fixed

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,5 +1,5 @@
 // import PropTypes from 'prop-types';
-import React , {useContext, useEffect} from 'react';
+import React , {useContext} from 'react';
 import { CartContext } from './Cart';
 import { useNavigate } from 'react-router-dom';
 
@@ -13,15 +13,16 @@ function Checkout() {
    navigate('/purchase');
    };
 
-  //  useEffect (() => {
-  //   clearCart();
-  //  },[]);
+  const completePurchase = () => {
+    clearCart();
+    navigatePurchase();
+  }
  
   return (
     <>
    
     <div className="flex-col flex items-center bg-white gap-8 p-10 text-black text-sm">
-        <h1 className="text-2xl font-bold">Cart</h1>
+        <h1 className="text-2xl font-bold">Cart Checkout</h1>
         <div className="absolute right-16 top-10">
       
     </div>
@@ -32,7 +33,7 @@ function Checkout() {
           <img src={item.image} alt={item.title} className="rounded-md h-24" />
           <div className="flex flex-col">
             <h1 className="text-lg font-bold">{item.title}</h1>
-            <p className="text-gray-600">{item.price}</p>
+            <p className="text-gray-600">${item.price.toFixed(2)}</p>
           </div>
     </div>
     <div className="flex gap-4">
@@ -50,11 +51,11 @@ function Checkout() {
     <h1 className="text-lg font-bold">Total: ${getCartTotal()}</h1>
     <button className="px-4 py-2 bg-gray-800 text-black text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
     onClick={() => {clearCart()}}> Clear cart </button>
-    </div>) : (<h1 className="text-lg font-bold">Your cart is empty</h1>)}
+    </div>) : (<h3 className="text-lg font-bold">Nothing in cart</h3>)}
 
 
-
-    <button className='done' onClick={navigatePurchase}>Complete Purchase</button>
+    {cartItems.length > 0 ? (
+    <button className='done' onClick={completePurchase}>Complete Purchase</button>) : (<h3 className="text-lg font-bold">Click home to start shopping!</h3>)}
     </div>
     
     
